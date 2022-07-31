3d5fffce96e20ee810751e43332e04fbad2d3290
adonis <adonis@kinvo.com.br>
2022-07-31 13:04:54 -0300

    test: ensures that data is being rendered

--- src/presentation/components/rick-and-morty-data-visualizer.spec.tsx
+++ src/presentation/components/rick-and-morty-data-visualizer.spec.tsx
@@ -1,7 +1,11 @@
+import { getByText, render, screen, waitFor } from "@testing-library/react";
+
 import { ResponseInterface } from "@/data/protocols";
 import { CharacterModel } from "@/domain";
 import { GetRickAndMortyData } from "@/domain/usecases";
 import { fakeData } from "@/tests";
+
+import { RickAndMortyDataVisualizer } from "./rick-and-morty-data-visualizer";
 
 class RemoteGetRickAndMortyDataMock implements GetRickAndMortyData {
   execute: (characterId: number) => Promise<ResponseInterface<CharacterModel>> =
@@ -19,3 +23,29 @@ class RemoteGetRickAndMortyDataMock implements GetRickAndMortyData {
       return Promise.resolve(response);
     };
 }
+
+function makeSut() {
+  const getRickAndMortyDataMock = new RemoteGetRickAndMortyDataMock();
+
+  render(
+    <RickAndMortyDataVisualizer
+      getRickAndMortyData={getRickAndMortyDataMock}
+    ></RickAndMortyDataVisualizer>
+  );
+
+  return { getRickAndMortyDataMock };
+}
+
+describe("<RickAndMortyDataVisualizer />", () => {
+  test("should render proper data", async () => {
+    makeSut();
+
+    const stringifiedFakeData = JSON.stringify(fakeData, null);
+
+    const dataVisualizer = await screen.findByText(stringifiedFakeData);
+
+    await waitFor(() => {
+      expect(dataVisualizer).toBeInTheDocument();
+    });
+  });
+});
